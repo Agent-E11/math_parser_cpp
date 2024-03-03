907e47ed84666a2fcfd51ca5fc4824cb960ac852
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-03-03 13:24:25 -0800

    Remove spaces from input, and loop over characters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
+#include <algorithm>
 #include <iostream>
-using namespace std;
+#include <string>
+#include <map>
 
 int main() {
-    cout << "Hello world" << endl;
+    std::map<char, int> precedence;
+    precedence['^'] = 4;
+    precedence['*'] = 3;
+    precedence['/'] = 3;
+    precedence['+'] = 2;
+    precedence['-'] = 2;
+
+    std::string input;
+
+    std::cout << "Input math expression: ";
+
+    std::getline(std::cin, input);
+
+    std::cout << "Input: " << input << std::endl;
+
+    { // Remove spaces
+        int len = input.length();
+
+        int space_count = std::count(input.begin(), input.end(), ' ');
+
+        auto _ = remove(input.begin(), input.end(), ' ');
+
+        input.resize(len - space_count);
+    }
+
+    std::cout << "Input: " << input << std::endl;
+
+    for (int i = 0; i < input.length(); i++) {
+        std::cout << i << ": " << input[i] << std::endl;
+    }
 }
