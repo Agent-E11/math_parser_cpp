e84717d73be8ceb58247ff0772bac3ddf53daf17
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-03-05 01:22:32 -0800

    Move infix-to-postfix logic to its own function

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void print_vec(std::vector<char> v) {
     for (int i = 0; i < v.size(); i++) {
         std::cout << v[i] << " ";
     }
-    //std::cout << std::endl;
+    std::cout << std::endl;
 }
 
 void print_map(std::map<char, int> m) {
@@ -24,7 +24,10 @@ bool map_contains(std::map<char, int> m, char c) {
     return m.find(c) != m.end();
 }
 
-int main() {
+std::vector<char> infix_to_postfix(std::vector<char> expr) {
+    std::vector<char> op_stack = {};
+    std::vector<char> out_queue = {};
+
     const std::map<char, int> precedence = {
         {'^', 4},
         {'*', 3},
@@ -32,32 +35,9 @@ int main() {
         {'+', 2},
         {'-', 2}
     };
-
-    std::string input;
-
-    std::vector<char> op_stack = {};
-    std::vector<char> out_queue = {};
-
-    std::cout << "Input math expression: ";
-
-    std::getline(std::cin, input);
-
-    std::cout << "Input: " << input << std::endl;
-
-    { // Remove spaces
-        int len = input.length();
-
-        int space_count = std::count(input.begin(), input.end(), ' ');
-
-        auto _ = remove(input.begin(), input.end(), ' ');
-
-        input.resize(len - space_count);
-    }
-
-    std::cout << "Input: " << input << std::endl;
-
-    for (int i = 0; i < input.length(); i++) {
-        char c = input[i];
+    
+    for (int i = 0; i < expr.size(); i++) {
+        char c = expr[i];
         // print_map(precedence);
         // std::cout << i << ": " << input[i] << "\t";
 
@@ -82,7 +62,7 @@ int main() {
             while (!op_stack.empty() && op_stack.back() != '(') {
                 if (op_stack.empty()) {
                     std::cout << "Error: mismatched parentheses" << std::endl;
-                    return 1;
+                    exit(1); // FIXME: Error should be handled better
                 }
                 out_queue.push_back(op_stack.back());
                 op_stack.pop_back();
@@ -95,17 +75,40 @@ int main() {
         std::cout << "\tQueue: "; print_vec(out_queue);
         std::cout << std::endl;
     }
-    // Add the rest of the operators to the output
-    std::cout << "moving operators" << std::endl;
-    while (!op_stack.empty()) {
-        if (op_stack.back() == '(' || op_stack.back() == ')') {
-            std::cout << "Error: mismatched parentheses" << std::endl;
-            return 1;
-        }
-        out_queue.push_back(op_stack.back());
-        op_stack.pop_back();
+
+    return out_queue;
+}
+
+int main() {
+    std::string input;
+
+    std::vector<char> op_stack = {};
+    std::vector<char> out_queue = {};
+
+    std::cout << "Input math expression: ";
+
+    std::getline(std::cin, input);
+
+    std::cout << "Input: " << input << std::endl;
+
+    { // Remove spaces
+        int len = input.length();
+
+        int space_count = std::count(input.begin(), input.end(), ' ');
+
+        auto _ = remove(input.begin(), input.end(), ' ');
+
+        input.resize(len - space_count);
     }
-    std::cout << "done moving operators" << std::endl;
-    std::cout << "Stack: "; print_vec(op_stack);
-    std::cout << "\t\t\tQueue: "; print_vec(out_queue);
+
+    std::cout << "Input: " << input << std::endl;
+
+    std::vector<char> infix_expr(input.begin(), input.end());
+
+    std::vector<char> postfix_expr = infix_to_postfix(infix_expr);
+
+    std::cout << "Infix: ";
+    print_vec(infix_expr);
+    std::cout << "Postfix: ";
+    print_vec(postfix_expr);
 }
