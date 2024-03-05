11c89933082170fde1a0c5a70fc72d17511dfd4b
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-03-05 01:50:02 -0800

    Add comments, remove unnecessary `cout`s

--- main.cpp
+++ main.cpp
@@ -25,9 +25,6 @@ bool map_contains(std::map<char, int> m, char c) {
 }
 
 std::vector<char> infix_to_postfix(std::vector<char> expr) {
-    std::vector<char> op_stack = {};
-    std::vector<char> out_queue = {};
-
     const std::map<char, int> precedence = {
         {'^', 4},
         {'*', 3},
@@ -36,17 +33,22 @@ std::vector<char> infix_to_postfix(std::vector<char> expr) {
         {'-', 2}
     };
     
+    std::vector<char> op_stack = {};
+    std::vector<char> out_queue = {};
+
     for (int i = 0; i < expr.size(); i++) {
         char c = expr[i];
-        // print_map(precedence);
-        // std::cout << i << ": " << input[i] << "\t";
 
         if (std::isalnum(c)) {
+            // If it is alphanumeric, push it to the output
             out_queue.push_back(c);
         } else if (map_contains(precedence, c)) {
-            std::cout << "c: " << c << "\t";
-            // std::cout << "p[c]: " << precedence.at(c) << "\t";
-            std::cout << "p.c: " << precedence.count(c) << "\t";
+            // If it is an operator
+
+            // Pop operators from the stack to the output, if:
+            // - The stack is not empty
+            // - The operator has a greater precedence
+            // - The operator is not '('
             while (
                 !op_stack.empty()
                 && op_stack.back() != '('
@@ -55,60 +57,63 @@ std::vector<char> infix_to_postfix(std::vector<char> expr) {
                 out_queue.push_back(op_stack.back());
                 op_stack.pop_back();
             }
+            // Add current operator to stack
             op_stack.push_back(c);
         } else if (c == '(') {
+            // If it is a '(', add it to the stack
             op_stack.push_back(c);
         } else if (c == ')') {
-            while (!op_stack.empty() && op_stack.back() != '(') {
-                if (op_stack.empty()) {
-                    std::cout << "Error: mismatched parentheses" << std::endl;
-                    exit(1); // FIXME: Error should be handled better
-                }
+            // If it is a ')'
+            
+            // Pop all operators to the stack until '('
+            while (
+                !op_stack.empty()
+                && op_stack.back() != '('
+            ) {
                 out_queue.push_back(op_stack.back());
                 op_stack.pop_back();
             }
+            // If the stack is empty, then there were mismatched parentheses
+            if (op_stack.empty()) {
+                std::cout << "Error: mismatched parentheses" << std::endl;
+                exit(1); // FIXME: Error should be handled better
+            }
             op_stack.pop_back(); // Remove '('
         } else {
-            std::cout << "is invalid" << std::endl;
+            // If it is none of the above conditions,
+            // then it is invalid, ignore it
+            std::cout << c << "is an invalid character. Ignoring" << std::endl;
         }
-        std::cout << "Stack: "; print_vec(op_stack);
-        std::cout << "\tQueue: "; print_vec(out_queue);
-        std::cout << std::endl;
     }
 
     return out_queue;
 }
 
 int main() {
-    std::string input;
-
     std::vector<char> op_stack = {};
     std::vector<char> out_queue = {};
 
+    std::string input;
     std::cout << "Input math expression: ";
-
     std::getline(std::cin, input);
-
-    std::cout << "Input: " << input << std::endl;
 
     { // Remove spaces
         int len = input.length();
-
         int space_count = std::count(input.begin(), input.end(), ' ');
-
         auto _ = remove(input.begin(), input.end(), ' ');
-
         input.resize(len - space_count);
     }
 
-    std::cout << "Input: " << input << std::endl;
-
+    // Convert string to vector<char>
     std::vector<char> infix_expr(input.begin(), input.end());
 
+    // Convert to postfix
     std::vector<char> postfix_expr = infix_to_postfix(infix_expr);
 
-    std::cout << "Infix: ";
-    print_vec(infix_expr);
-    std::cout << "Postfix: ";
-    print_vec(postfix_expr);
+    // Convert vector<char> to string
+    std::string postfix_str(postfix_expr.begin(), postfix_expr.end());
+
+
+    std::cout << "Infix: " << input << std::endl;
+    std::cout << "Postfix: " << postfix_str << std::endl;
 }
